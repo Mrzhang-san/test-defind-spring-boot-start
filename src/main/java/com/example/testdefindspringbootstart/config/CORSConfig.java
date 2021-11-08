c68f89abec67838721b9ba8b08ae164cb4fe25e5
username <useremail@qq.com>
2021-11-08 17:40:26 +0800

    Spring Boot 配置 Jetty 容器

---
+++ src/main/java/com/example/testdefindspringbootstart/config/CORSConfig.java
@@ -0,0 +1,22 @@
+package com.example.testdefindspringbootstart.config;
+
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 配置跨域访问
+ */
+public class CORSConfig implements WebMvcConfigurer {
+   /* @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //允许所有跨域访问
+        registry.addMapping("/**");
+    }*/
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                //.allowedOrigins("http://localhost")
+                .allowedMethods("POST", "GET");
+    }
+}
